8b7245ebf24aa34e3ea8b54a6d3806fa3ab3b220
unknown <swadeshjain786@gmail.com>
2025-01-23 00:32:37 +0530

    pokedex

---
+++ src/Components/Search/Search.jsx
@@ -0,0 +1,10 @@
+import './Search.css';
+
+
+function Search(){
+return(
+
+    <input id='search-pokemon' type="text" placeholder='Which pokemaon you are looking for' />
+)
+}
+export default Search;
